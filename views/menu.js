3bf354890613c3b415fd32a8ab723fd5b2ebe8c5
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2021-05-08 16:56:12 +1000

    add menu message

---
+++ views/menu.js
@@ -0,0 +1,9 @@
+function menu() {
+  return `
+    Select view options:
+    * Press 1 to view all tickets
+    * Press 2 to view a ticket
+    * Type "quit" to exit`;
+}
+
+module.exports = { menu };
