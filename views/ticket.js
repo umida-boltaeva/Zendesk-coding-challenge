7040e25c80fe94a226f90e95c6617f5a084b6ad9
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2021-05-08 16:57:58 +1000

    create a single ticket table

---
+++ views/ticket.js
@@ -0,0 +1,28 @@
+const Table = require("cli-table3");
+const { menu } = require("./menu");
+
+function ticket(ticketData) {
+  var table = new Table();
+  const ticket = ticketData.data.ticket;
+
+  table.push(
+    { id: ticket.id },
+    { url: ticket.url },
+    { created_at: ticket.created_at },
+    { updated_at: ticket.updated_at },
+    { type: ticket.type },
+    { subject: ticket.subject },
+    { priority: ticket.priority },
+    { status: ticket.status },
+    { has_incidents: ticket.has_incidents },
+    { is_public: ticket.is_public },
+    { tags: ticket.tags.reduce((prev, curr) => `${prev}, ${curr}`) }
+  );
+
+  //ticket.description is too long for the table. So, I put it at the end of the table.
+  return `${table.toString()}
+  ${ticket.description}
+  ${menu()}`;
+}
+
+module.exports = { ticket };
